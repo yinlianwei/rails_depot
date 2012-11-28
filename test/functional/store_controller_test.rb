9ddd186492b02d3ec6e30a9b00b45b9438db9695
yinlianwei <yinlianwei@gmail.com>
2012-11-28 22:41:09 +0800

    C4迭代，完善目录

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get index" do	
     get :index
     assert_response :success
+    assert_response '#columns #side a', :minimum => 4
+    assert_response '#main .entry', 3
+    assert_response 'h3','Programing Ruby 1.9'
+    assert_response '.price', /\$[,\d]+\.\d\d/
   end
 
 end
